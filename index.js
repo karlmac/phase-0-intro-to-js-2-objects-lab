322705134f4f9afdfaec599ff16a770efd2171ac
Karlmac Yerima <yerimakarlmac@gmail.com>
2023-03-11 02:16:17 -0600

    edits index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Mark K",
+    streetAddress: "123 E Venier Blvd, Low Rapids, PK, 47015"
+}
+
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newEmp = {...obj
+        , [key]: value
+    }
+    //newEmp[key].value = value;
+
+    return newEmp;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newEmp = {...obj}
+    delete newEmp[key];
+    
+    return newEmp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    
+    return obj;
+}
